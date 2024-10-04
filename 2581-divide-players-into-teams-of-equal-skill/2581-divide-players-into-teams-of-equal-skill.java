2ec70586ddab4256177f31514407a727a721e915
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2024-10-04 23:40:34 +0530

    Time: 2460 ms (5.54%), Space: 56.4 MB (92.44%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.java
@@ -1,68 +1,76 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        // Set<List<Integer>> ls = new HashSet<>();
-        // int n = skill.length;
-        // int sum = 0;
-        // for(int i = 0;i<n;i++){
-        //      sum+=skill[i];
-        // }
-        // int pairs = n/2;
-        // int cont = sum/pairs;
-        // if((sum%cont)!=0){
-        //     return -1;
-        // }
-        // for(int i = 0;i<n;i++){
-        //     for(int j = i;j<n;j++){
-        //           if(skill[i]+skill[j] == cont){
-        //                 List<Integer> l = Arrays.asList(skill[i], skill[j]);
-        //                 Collections.sort(l);
-        //                 ls.add(l);
-        //           }
-        //     }
-        // }
-        // System.out.println(ls);
-        // int chem = 0;
+        ArrayList<List<Integer>> ls = new ArrayList<>();
+        int n = skill.length;
+        int sum = 0;
+        for(int i = 0;i<n;i++){
+             sum+=skill[i];
+        }
+        int pairs = n/2;
+        int cont = sum/pairs;
+        if((sum%cont)!=0){
+            return -1;
+        }
+           long chem = 0;
+        boolean used[] = new boolean[n];
+        for(int i = 0;i<n;i++){
+            if(used[i]) continue;
+            for(int j = i+1;j<n;j++){
+                  if( !used[j] && skill[i]+skill[j] == cont){
+                        chem += skill[i] * skill[j];
+                        List<Integer> l = Arrays.asList(skill[i], skill[j]);
+                        Collections.sort(l);
+                        ls.add(l);
+                        used[i] = true;
+                        used[j] = true;
+                        break;
+                  }
+            }
+        }
+        for (boolean u : used) {
+             if (!u) return -1;
+        }
         // for(List<Integer> l : ls){
         //     chem+=(l.get(0)*l.get(1));
         // }
-        // return chem;
-          Set<List<Integer>> ls = new HashSet<>();
-        int n = skill.length;
-        int sum = 0;
-
-        for (int i = 0; i < n; i++) {
-            sum += skill[i];
-        }
-
-        int pairs = n / 2;
-        int cont = sum / pairs;
-
-        if (sum % pairs != 0) {
-            return -1;
-        }
-
-        boolean[] used = new boolean[n];
-        long chem = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (used[i]) continue;
-
-            for (int j = i + 1; j < n; j++) {
-                if (!used[j] && skill[i] + skill[j] == cont) {
-                    chem += skill[i] * skill[j];
-                    ls.add(Arrays.asList(skill[i], skill[j]));
-                    used[i] = true;
-                    used[j] = true;
-                    break;
-                }
-            }
-        }
-
-        for (boolean u : used) {
-            if (!u) return -1;
-        }
-
-        System.out.println(ls);
         return chem;
+        //   Set<List<Integer>> ls = new HashSet<>();
+        // int n = skill.length;
+        // int sum = 0;
+
+        // for (int i = 0; i < n; i++) {
+        //     sum += skill[i];
+        // }
+
+        // int pairs = n / 2;
+        // int cont = sum / pairs;
+
+        // if (sum % pairs != 0) {
+        //     return -1;
+        // }
+
+        // boolean[] used = new boolean[n];
+        // long chem = 0;
+
+        // for (int i = 0; i < n; i++) {
+        //     if (used[i]) continue;
+
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (!used[j] && skill[i] + skill[j] == cont) {
+        //             chem += skill[i] * skill[j];
+        //             ls.add(Arrays.asList(skill[i], skill[j]));
+        //             used[i] = true;
+        //             used[j] = true;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // for (boolean u : used) {
+        //     if (!u) return -1;
+        // }
+
+        // System.out.println(ls);
+        // return chem;
     }
 }
