b33b6850a1d3d12362059ae7cf786e953af68d97
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2024-12-12 21:22:50 +0530

    Time: 5 ms (89.83%), Space: 42.7 MB (21.79%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.java
@@ -1,14 +1,27 @@
 class Solution {
     public long pickGifts(int[] gifts, int k) {
-        int n = gifts.length;
-        long totalSum = 0;
-        while(k-- > 0){
-            Arrays.sort(gifts);
-            gifts[n-1] = (int)Math.sqrt(gifts[n-1]);
+        // int n = gifts.length;
+        // long totalSum = 0;
+        // while(k-- > 0){
+        //     Arrays.sort(gifts);
+        //     gifts[n-1] = (int)Math.sqrt(gifts[n-1]);
+        // }
+        // for(int i = 0;i<n;i++){
+        //     totalSum+=gifts[i];
+        // }
+        // return totalSum;
+        PriorityQueue<Integer> mh = new PriorityQueue<>(Collections.reverseOrder()); 
+        // maxheap remember this one sid !
+        long sum = 0;
+        for(int val : gifts){
+            mh.add(val);
         }
-        for(int i = 0;i<n;i++){
-            totalSum+=gifts[i];
+        for(int i = 0;i<k;i++){
+            mh.add((int)Math.sqrt(mh.poll()));
         }
-        return totalSum;
+        for(int val : mh){
+            sum+=val;
+        }
+        return sum;
     }
 }
