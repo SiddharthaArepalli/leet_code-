6a8857aa603cd981069153889d0ea1eb02ae0ca5
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2024-12-19 10:47:53 +0530

    Time: 1 ms (98.94%), Space: 43.9 MB (38.24%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,30 +1,40 @@
 class Solution {
     public int[] finalPrices(int[] prices) {
         int n = prices.length;
-        int i = 0;
-        int j = i+1;
-        int flag = 0;
-        while(i < n-1){
-            if(prices[j]<= prices[i]){
-                prices[i] = prices[i]-prices[j];
-                if(flag == 1){
-                    i++;
-                    j=i+1;
-                    flag = 0;
-                }else{
-                    i++;
-                    j++;
+        // int i = 0;
+        // int j = i+1;
+        // int flag = 0;
+        // while(i < n-1){
+        //     if(prices[j]<= prices[i]){
+        //         prices[i] = prices[i]-prices[j];
+        //         if(flag == 1){
+        //             i++;
+        //             j=i+1;
+        //             flag = 0;
+        //         }else{
+        //             i++;
+        //             j++;
+        //         }
+        //     }
+        //     else{
+        //        flag = 1;
+        //        j++;
+        //        if(j == n){
+        //          i++;
+        //          j = i+1;
+        //        }
+        //     }
+        // }
+        
+        for(int i = 0;i<n;i++){
+            for(int j =i+1;j<n;j++){
+                if(prices[j]<= prices[i]){
+                    prices[i]-=prices[j];
+                    break;
                 }
             }
-            else{
-               flag = 1;
-               j++;
-               if(j == n){
-                 i++;
-                 j = i+1;
-               }
-            }
         }
+
         return prices;
     }
 }
