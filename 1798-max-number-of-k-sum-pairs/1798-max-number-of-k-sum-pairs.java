d3221b9fe4b22ed64b27d1d7c692ec02cf727469
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2025-01-19 21:42:57 +0530

    Time: 18 ms (93.63%), Space: 57.3 MB (45.23%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -1,32 +1,23 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
         int n = nums.length;
-        // List<Integer> ls = new ArrayList<>();
-        // for(int num : nums){
-        //     ls.add(num);
-        // }
-        // int c = 0;
-        // for(int i = 0;i<n;i++){
-        //     int find = k - nums[i];
-        //     if(ls.contains(find)){
-        //         ls.remove(Integer.valueOf(find));
-        //         ls.remove(Integer.valueOf(nums[i]));
-        //         c++;
-        //     }
-        // }
-        // return c;
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        Arrays.sort(nums);
+        int l = 0;
+        int r = n-1;
         int c = 0;
-        for(int num : nums){
-            int find = k - num;
-            if(hm.getOrDefault(find,0) > 0){
-                c++;
-                hm.put(find,hm.get(find)-1);
+        while(l<r){
+            int sum = nums[l]+nums[r];
+            if(sum == k){
+              c++;
+              l++;
+              r--;
+            }
+            else if(sum < k){
+                l++;
             }
             else{
-                hm.put(num,hm.getOrDefault(num,0)+1);
+                r--;
             }
-
         }
         return c;
     }
