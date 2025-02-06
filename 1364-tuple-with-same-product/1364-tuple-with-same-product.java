17e2608f36ca78bd4392ad9dce3f625803a12258
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2025-02-06 21:19:48 +0530

    Time: 401 ms (5.77%), Space: 92.8 MB (5.13%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.java
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.java
@@ -1,13 +1,12 @@
 class Solution {
-    public static long nCr(int n, int r)
+public static long nCr(int n, int r)
 {
-    return fact(n) / (fact(r) *
-                  fact(n - r));
+    return fact(n) / (fact(r) * fact(n - r));
 }
 public static long fact(int n)
 {
-      if(n==0 || n==1)
-      return 1;
+    if(n==0 || n==1)
+    return 1;
     long res = 1;
     for (int i = 2; i <= n; i++)
         res = res * i;
@@ -15,20 +14,18 @@ public static long fact(int n)
 }
     public int tupleSameProduct(int[] nums) {
         int n = nums.length;
+        int ans = 0;
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i = 0 ;i<n;i++){
             for(int j = i+1;j<n;j++){
                 int pro = nums[i]*nums[j];
+                if(hm.containsKey(pro)){
+                    ans+=hm.get(pro);
+                }
                 hm.put(pro, hm.getOrDefault(pro,0)+1);
             }
         }
-        int ans = 0;
-        for(int i : hm.values()){
-            if(i>1){
-                ans+=nCr(i,2)*(8);
-            }
-        }
         System.out.println(hm);
-        return ans;
+        return ans*8;
     }
 }
