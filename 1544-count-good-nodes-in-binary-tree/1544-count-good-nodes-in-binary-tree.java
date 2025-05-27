c7f752cb70ca8812106ef345567ab4911292adbe
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2025-05-27 23:54:17 +0530

    Time: 2 ms (85.12%), Space: 52.2 MB (75.92%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.java
@@ -13,20 +13,37 @@
  *     }
  * }
  */
+// class Solution {
+//     int count = 0;
+//     public void dfs(TreeNode root , int maxSeen){
+//         if(root == null) return;
+//         if(root.val >= maxSeen){ 
+//             count++;
+//             maxSeen = root.val;
+//         }
+//         dfs(root.left , maxSeen);
+//         dfs(root.right, maxSeen);
+//     }
+//     public int goodNodes(TreeNode root) {
+//         int maxSeen = Integer.MIN_VALUE;
+//         dfs(root , maxSeen);
+//         return count;
+//     }
+// }
+
 class Solution {
-    int count = 0;
-    public void dfs(TreeNode root , int maxSeen){
-        if(root == null) return;
-        if(root.val >= maxSeen){ 
-            count++;
+    public int dfs(TreeNode root, int maxSeen) {
+        if (root == null) return 0;
+        int count = 0;
+        if (root.val >= maxSeen) {
+            count = 1;
             maxSeen = root.val;
         }
-        dfs(root.left , maxSeen);
-        dfs(root.right, maxSeen);
+        return count + dfs(root.left, maxSeen) + dfs(root.right, maxSeen);
     }
+
     public int goodNodes(TreeNode root) {
         int maxSeen = Integer.MIN_VALUE;
-        dfs(root , maxSeen);
-        return count;
+        return dfs(root, maxSeen);
     }
 }
