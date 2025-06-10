429061eb489f2d4d1faf787bf7804150eb63b9c8
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2025-06-11 00:14:38 +0530

    Time: 10 ms (5.31%), Space: 45 MB (21.35%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,20 +1,25 @@
 class Solution {
-    public static void cbSum(int cand[] , int tar , List<List<Integer>> res , List<Integer> cur , int ind){
+    static HashSet<List<Integer>> hs = new HashSet<>();
+    public void cbSum(int cand[] , int tar , List<List<Integer>> res , List<Integer> cur , int ind){
             int n = cand.length;
             if(ind == n || tar<0) return;
             if(tar == 0) {
-                res.add(new ArrayList<>(cur));
+                if(!hs.contains(cur)) {
+                Collections.sort(new ArrayList<>(cur));
+                    hs.add(cur); 
+                    res.add(new ArrayList<>(cur));
+                }
                 return;
             }
             cur.add(cand[ind]);
-            // cbSum(cand , tar-cand[ind] , res , cur ,ind+1);
+            cbSum(cand , tar-cand[ind] , res , cur ,ind+1);
             cbSum(cand , tar-cand[ind] , res , cur,  ind);
             cur.remove(cur.size()-1);
             cbSum(cand , tar , res , cur , ind+1);
     }
-    public List<List<Integer>> combinationSum(int[] cand, int tar) {
+    public List<List<Integer>> combinationSum(int[] cand, int tar) { 
         List<List<Integer>> res = new ArrayList<>();
-        cbSum(cand , tar ,res , new ArrayList<>() , 0);
+        cbSum(cand , tar , res , new ArrayList<>() , 0);
         return res;
     }
 }
