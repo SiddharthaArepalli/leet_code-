35610cf0491089ea3cc39454d074db19b4184285
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2025-06-21 16:51:38 +0530

    Time: 9 ms (47.15%), Space: 46.7 MB (39.2%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,24 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intv) {
-        int n = intv.length;
-        Arrays.sort(intv, (a,b)->a[0]-b[0]);
-        int res[][] = new int[n][2];
-        int pAt = intv[0][0];
-        int pDt = intv[0][1];
-        res[0] = new int[]{pAt, pDt}; 
-        int ind = 0;
-        for(int i = 1;i<n;i++){
-            if(pDt>=intv[i][0]){
-                pDt = Math.max(pDt, intv[i][1]);
-                res[ind][1] = pDt;
-            }
-            else{
-                ind++;
-                res[ind]=new int[]{intv[i][0],intv[i][1]};
-                pAt = intv[i][0];
-                pDt = intv[i][1];
-            }
-        }
-        return Arrays.copyOf(res,ind+1);
+         int n = intv.length;
+         List<int[]> res = new ArrayList<>();
+         Arrays.sort(intv, (a,b)->(a[0]-b[0]));
+         int pAt = intv[0][0];
+         int pDt = intv[0][1];
+         res.add(new int[]{pAt , pDt});
+         for(int i = 1;i<n;i++){
+              int cAt = intv[i][0];
+              int cDt = intv[i][1];
+              int last[] = res.get(res.size()-1);
+              if(cAt <= pDt){
+                   last[1] = Math.max(last[1] , cDt);
+                   pDt = Math.max(last[1] , cDt);
+              }
+              else{
+                   res.add(new int[]{cAt ,cDt});
+                   pAt = cAt;
+                   pDt = cDt;
+              }
+         }
+         return res.toArray(new int[res.size()][2]);
     }
 }
