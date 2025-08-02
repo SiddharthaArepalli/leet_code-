8ddaecaef2705a68fcfaaa9267b8b656c6fcec55
SiddharthaArepalli <140429954+SiddharthaArepalli@users.noreply.github.com>
2025-08-02 12:34:58 +0530

    Time: 1 ms (62.18%), Space: 46.4 MB (49.61%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,20 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int prefixSum[] = new int[n];
-        int suffixSum[] = new int[n];
-        prefixSum[0] = height[0];
-        for(int i = 1;i<n;i++){
-            prefixSum[i] = Math.max(prefixSum[i-1] ,height[i]);
-        }
-        suffixSum[n-1]= height[n-1];
+         int n = height.length;
+         int prefMax[] = new int[n];
+         int sufMax[] = new int[n];
+         prefMax[0] = height[0];
+         for(int i = 1;i<n;i++){
+              prefMax[i] = Math.max(prefMax[i-1],height[i]); 
+         }
+         sufMax[n-1] = height[n-1];
          for(int i = n-2;i>=0;i--){
-            suffixSum[i] = Math.max(suffixSum[i+1] , height[i]);
-        }
-        int maxWater = 0;
-        for(int i = 0;i<n;i++){
-            maxWater+= Math.min(prefixSum[i],suffixSum[i]) - height[i];
-           
-        }
-   return maxWater;
-        
+              sufMax[i] = Math.max(sufMax[i+1], height[i]);
+         }
+         int ans = 0;
+         for(int i = 0;i<n;i++){
+              ans+=Math.abs(Math.min(prefMax[i] , sufMax[i]) - height[i]);
+         }
+         return ans;
     }
 }
